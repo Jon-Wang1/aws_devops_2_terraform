4e4ba8b1dae607a7a650036b779bac4f7ce1fcf4
collinsctk@qytang.com <collinsctk@qytang.com>
2021-01-07 10:12:26 +0800

    ec2添加route53

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "instance_ip_addr" {
-  value = aws_instance.amazon_linux_2.private_ip
+  value = aws_instance.qytang_ec2.private_ip
 }
 
 output "instance_public_ip_addr" {
-  value = aws_instance.amazon_linux_2.public_ip
+  value = aws_instance.qytang_ec2.public_ip
 }
 
 output "instance_public_sg" {
-  value = aws_instance.amazon_linux_2.security_groups
+  value = aws_instance.qytang_ec2.security_groups
 }
