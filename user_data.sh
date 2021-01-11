da8899002cd5741e0dab303b5aa077bebf383247
collinsctk@qytang.com <collinsctk@qytang.com>
2021-01-11 09:06:25 +0800

    切换目录！

--- user_data.sh
+++ user_data.sh
@@ -16,6 +16,7 @@ service sshd restart
 # git下载项目
 yum install -y git
 # aws s3 cp s3://qytangawss3/.gitconfig ~/.gitconfig
+cd ~
 git config --global credential.helper '!aws codecommit credential-helper $@'
 git config --global credential.UseHttpPath true
 sudo git clone https://git-codecommit.ap-northeast-2.amazonaws.com/v1/repos/aws_flask /aws_flask
